0e6c865ea66f635ed77a98aaee536e102adafdac
Naomi <naomilai601@gmail.com>
2025-04-22 19:05:43 -0400

    minor changes to graphs

--- app/static/js/stats.js
+++ app/static/js/stats.js
@@ -3,46 +3,50 @@ import {
 } from "./charts.js"; 
 
 const sleep_means = document.getElementById('sleep_means');
-new Chart(sleep_means, {
-  type: 'line',
-  data: {
-    labels: r(sleep_data), 
-    datasets: [
-      {
-      label: 'avg anxiety level',
-      data: mean(anx_data, sleep_data),
-      }
-    ],
-  },
-  opt: opt,
-});
+  new Chart(sleep_means, {
+    type: 'line',
+    data: {
+      labels: r(sleep_data), 
+      datasets: [
+        {
+        label: 'avg anxiety level',
+        data: mean(anx_data, sleep_data),
+        }
+      ],
+    },
+  });
 
   const  occupation_means = document.getElementById('occupation_means');
   new Chart(occupation_means,{
     type: 'bar',
     data: {
-      labels: r(occupation_data),
+      labels: s(r(occupation_data), mean(anx_data, occupation_data)),
       datasets: [{
         label: 'avg anxiety level',
-        data: mean(anx_data, occupation_data),
+        data: r(mean(anx_data, occupation_data)),
       }]
     },
   })
   
 
-    const therapy_means = document.getElementById('therapy_means');
-    new Chart(therapy_means, {
-      type: 'line',
-      data: {
-        labels: r(therapy_data), 
-        datasets: [
-          {
-          label: 'avg anxiety level',
-          data: mean(anx_data, therapy_data),
-          }
-        ],
-      },
-      options: {
-        animation: false,
-      },
-    });
+  const therapy_means = document.getElementById('therapy_means');
+  new Chart(therapy_means, {
+    type: 'line',
+    data: {
+      labels: r(therapy_data), 
+      datasets: [
+        {
+        label: 'avg anxiety level',
+        data: mean(anx_data, therapy_data),
+        }
+      ],
+    },
+    options: {
+      animation: false,
+      scales: {
+        y: {
+            suggestedMin: 0,
+            suggestedMax: 10
+        }}
+    },
+  });
